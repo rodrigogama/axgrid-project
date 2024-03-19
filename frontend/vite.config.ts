c70360f7d9519217a05282c8148888a25b443831
Rodrigo Gama <ro.gama91@gmail.com>
2024-03-19 23:21:15 +0100

    chore(frontend): add vitest configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./setupTests.ts",
+    css: true,
+    coverage: {
+      reporter: ["text", "json", "html"],
+      include: ["src"],
+      exclude: [
+        "src/App.tsx",
+        "src/domains",
+        "src/__mocks__",
+        "src/styles",
+        "src/**/{index,main,test-utils}.{ts,tsx}",
+        "src/**/types.{ts,tsx}",
+        "src/**/*.d.ts",
+      ],
+    },
+  },
+});
