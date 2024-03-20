f624ea8047249d433ad277ca358dea2905bab616
Rodrigo Gama <ro.gama91@gmail.com>
2024-03-20 01:23:02 +0100

    tests(utils): utils for rendering with providers when testing

---
+++ frontend/src/test-utils.tsx
@@ -0,0 +1,66 @@
+import { ReactElement, ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { render, RenderOptions } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
+
+interface RenderQueryHookOptions {
+  queryClient?: QueryClient;
+}
+
+const defaultTestQueryClientOptions = {
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: Infinity,
+    },
+  },
+};
+
+export function renderQueryHook<T>(
+  hook: () => T,
+  { queryClient }: RenderQueryHookOptions = {}
+) {
+  const client = queryClient || new QueryClient(defaultTestQueryClientOptions);
+
+  const wrapper = ({ children }: { children: ReactNode }) => (
+    <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  );
+
+  return renderHook(hook, { wrapper });
+}
+
+export const renderWithProviders = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "queries"> & RenderQueryHookOptions
+) => {
+  const client =
+    options?.queryClient || new QueryClient(defaultTestQueryClientOptions);
+
+  return {
+    ...render(
+      <QueryClientProvider client={client}>{ui}</QueryClientProvider>,
+      options
+    ),
+  };
+};
+
+export const renderWithRouterAndProviders = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "queries"> & RenderQueryHookOptions,
+  path?: {
+    path: string;
+    url: string;
+  }
+) => {
+  return {
+    ...renderWithProviders(
+      <BrowserRouter>
+        <Routes>
+          <Route path={path?.path || ""} element={ui} />
+        </Routes>
+      </BrowserRouter>,
+      options
+    ),
+  };
+};
