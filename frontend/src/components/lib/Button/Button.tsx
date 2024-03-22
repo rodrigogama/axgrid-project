bcd17251709de36d6da1de7758d74b5379ed8c61
Rodrigo Gama <ro.gama91@gmail.com>
2024-03-22 20:48:54 +0100

    fix(components): fixed button html props + disabled style

--- frontend/src/components/lib/Button/Button.tsx
+++ frontend/src/components/lib/Button/Button.tsx
@@ -1,13 +1,15 @@
 import clsx from "clsx";
 
-export const Button = ({ className, variant, ...props }: Props) => {
+export const Button = ({ className, variant, disabled, ...props }: Props) => {
   return (
     <button
       className={clsx("btn", className, {
         btn__primary: variant === "primary",
         btn__secondary: variant === "secondary",
         btn__link: variant === "link",
+        btn__disabled: disabled,
       })}
+      disabled={disabled}
       {...props}
     />
   );
@@ -15,4 +17,4 @@ export const Button = ({ className, variant, ...props }: Props) => {
 
 type Props = {
   variant: "primary" | "secondary" | "link";
-} & React.HTMLAttributes<HTMLButtonElement>;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
