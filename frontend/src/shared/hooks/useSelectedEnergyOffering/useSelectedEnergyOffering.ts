bc1f977c83f14fae27edde634b3dc767b2b5df3a
Rodrigo Gama <ro.gama91@gmail.com>
2024-03-22 20:49:31 +0100

    feat(hooks): make useSelectedEnergyOffering generic and type safe

--- frontend/src/shared/hooks/useSelectedEnergyOffering/useSelectedEnergyOffering.ts
+++ frontend/src/shared/hooks/useSelectedEnergyOffering/useSelectedEnergyOffering.ts
@@ -1,8 +1,11 @@
 import { useContext } from "react";
 import { EnergyOfferingContext } from "../../contexts/EnergyOfferingContext";
+import { TEnergyOfferingContext } from "../../contexts/EnergyOfferingContext/types";
 
-export const useSelectedEnergyOffering = () => {
-  const context = useContext(EnergyOfferingContext);
+export const useSelectedEnergyOffering = <T>() => {
+  const context = useContext<TEnergyOfferingContext<T> | undefined>(
+    EnergyOfferingContext
+  );
 
   if (context === undefined) {
     throw new Error(
