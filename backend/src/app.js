c63b54a3657a95a70c6fc88427c9d215754f2ced
Rodrigo Gama <ro.gama91@gmail.com>
2024-03-24 00:30:03 +0100

    feat(api): implemented socket.io on post api routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,15 @@ const { createServer } = require('http');
 const { Server } = require('socket.io');
 const jsonServer = require('json-server');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
 const port = 3001;
 
 // JSON server router
@@ -20,31 +25,50 @@ const customRoutes = {
   '/api/energy-offerings': '/energyOfferings',
 };
 
-const saveEnergyOfferingMiddleware = (req, res, next) => {
-  if (req.method === 'POST' && req.path === '/api/energy-offerings') {
-    req.body.status = 'PENDING';
-  }
-
-  next();
-};
-
-// use default middlewares
+app.use(cors());
+app.use((req, res, next) => {
+  // sets default timeout just to simulate real api calls
+  setTimeout(next, 500);
+});
 app.use(middlewares);
 app.use(jsonServer.bodyParser); // handle POST, PUT and PATCH
-app.use(saveEnergyOfferingMiddleware);
+app.post('/api/energy-offerings', (req, res) => {
+  const db = router.db;
+  const offeringsTable = db.get('energyOfferings');
+
+  const newId = offeringsTable.reduce((acc, item) => Math.max(acc, item.id || 0), 0) + 1;
+  const newOffering = { ...req.body, id: newId, status: 'OPEN' };
+  const result = offeringsTable.push(newOffering).last().write();
+
+  io.emit('offerings:newOffering', newOffering);
+  res.status(201).send(result);
+});
+app.post('/api/energy-offerings/:id/buy', async (req, res) => {
+  console.log('Route hit');
+  const { id } = req.params;
+  const parsedId = Number(id);
+  const db = router.db;
+  const offeringsTable = db.get('energyOfferings');
+
+  offeringsTable.find({ id: parsedId }).assign({ status: 'PROCESSING' }).write();
+  const updatedOffering = offeringsTable.find({ id: parsedId }).value();
+
+  io.emit('offerings:statusProcessing', updatedOffering);
+
+  // simulates an async event processing the request
+  setTimeout(() => {
+    offeringsTable.find({ id: parsedId }).assign({ status: 'ACCEPTED' }).write();
+    const updatedOffering = offeringsTable.find({ id: parsedId }).value();
+
+    io.emit('offerings:statusAccepted', updatedOffering);
+  }, 1500);
+
+  res.status(200).send(updatedOffering);
+});
 app.use(jsonServer.rewriter(customRoutes));
 app.use(router);
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-
-  // Example of emitting a message to clients
-  socket.emit('welcome', { message: 'Welcome to the Real-time API' });
-});
+httpServer.setTimeout(10000);
 
 httpServer.listen(port, () => {
   console.log(`JSON Server is running on http://localhost:${port}`);
