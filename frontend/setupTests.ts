791a8c9f803849a2b18803e995dca1fe224463b4
Rodrigo Gama <ro.gama91@gmail.com>
2024-03-23 15:13:41 +0100

    tests(setup): add resize observer polyfill

--- frontend/setupTests.ts
+++ frontend/setupTests.ts
@@ -1,12 +1,15 @@
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import ResizeObserver from "resize-observer-polyfill";
 import { server } from "./src/__mocks__/server";
 
 global.console = {
   ...console,
   error: vi.fn(),
 };
+
+global.ResizeObserver = ResizeObserver;
 
 beforeAll(() => {
   server.listen();
