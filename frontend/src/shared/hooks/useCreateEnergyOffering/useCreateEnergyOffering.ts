9d9208243349237165ba0c05506595c48e8fc4ec
Rodrigo Gama <ro.gama91@gmail.com>
2024-03-24 11:57:04 +0100

    fix(hooks): update list after creating an energy offering

--- frontend/src/shared/hooks/useCreateEnergyOffering/useCreateEnergyOffering.ts
+++ frontend/src/shared/hooks/useCreateEnergyOffering/useCreateEnergyOffering.ts
@@ -1,13 +1,21 @@
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import {
   EnergyOfferingService,
   EnergyOfferingRequest,
 } from "../../../services/api/energy-offerings";
+import { QUERY_KEYS } from "../../constants";
 
 export const useCreateEnergyOffering = () => {
+  const queryClient = useQueryClient();
+
   const { mutate, ...mutation } = useMutation({
     mutationFn: ({ data }: Props) => {
       return EnergyOfferingService.save(data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.ENERGY_OFFERINGS_LIST],
+      });
     },
   });
 
