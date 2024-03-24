a0862a5e096ead09c041c119031bb6b75bb0ae97
Rodrigo Gama <ro.gama91@gmail.com>
2024-03-24 02:52:19 +0100

    fix(hooks): remove unnecessary query invalidation

--- frontend/src/shared/hooks/useCreateEnergyOffering/useCreateEnergyOffering.ts
+++ frontend/src/shared/hooks/useCreateEnergyOffering/useCreateEnergyOffering.ts
@@ -1,21 +1,13 @@
-import { useMutation, useQueryClient } from "react-query";
+import { useMutation } from "react-query";
 import {
   EnergyOfferingService,
   EnergyOfferingRequest,
 } from "../../../services/api/energy-offerings";
-import { QUERY_KEYS } from "../../constants";
 
 export const useCreateEnergyOffering = () => {
-  const queryClient = useQueryClient();
-
   const { mutate, ...mutation } = useMutation({
     mutationFn: ({ data }: Props) => {
       return EnergyOfferingService.save(data);
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: [QUERY_KEYS.ENERGY_OFFERINGS_LIST],
-      });
     },
   });
 
